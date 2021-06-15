1b59663d681b547610d5b87de2c02384cef54700
jazibsawar <jazibsawar@gmail.com>
2021-06-15 21:12:12 +0300

    edit: make gasPrice optional

--- caterpillar-full (REST API - backend)/caterpillar-core/src/adapters/ethereum-blockchain/structs/account-info.ts
+++ caterpillar-full (REST API - backend)/caterpillar-core/src/adapters/ethereum-blockchain/structs/account-info.ts
@@ -1,10 +1,10 @@
 
 export class AccountInfo {
     from: any;
-    gas: any;
-    gasPrice: any;
+    gas?: any;
+    gasPrice?: any;
 
-    constructor(from: any, gas: any, gasPrice: any) {
+    constructor(from: any, gas?: any, gasPrice?: any) {
         this.from = from;
         this.gas = gas;
         this.gasPrice = gasPrice;
