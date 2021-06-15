179c07b05ba0fdf6d4449f0e75ce404f93699766
jazibsawar <jazibsawar@gmail.com>
2021-06-15 21:13:42 +0300

    add: refactor & hyperledger endpoints

--- caterpillar-full (REST API - backend)/caterpillar-core/src/runtime-registry/routes/index.ts
+++ caterpillar-full (REST API - backend)/caterpillar-core/src/runtime-registry/routes/index.ts
@@ -2,9 +2,14 @@ import { Router } from 'express';
 const router: Router = Router();
 
 import * as registryCtrl from '../controllers/registry-controller';
+import * as registryHyperledgerCtrl from '../controllers/registry-hyperledger-controller';
 
 router.post('/registries', registryCtrl.deployRuntimeRegistry);
 router.get('/registries/:registryAddress/address', registryCtrl.getRuntimeRegistryInfo);
 router.get('/registries/:registryId', registryCtrl.getRuntimeRegistryInfo);
+
+router.post('/hyperledger/registries', registryHyperledgerCtrl.deployRuntimeRegistry);
+router.get('/hyperledger/registries/:registryAddress/address', registryHyperledgerCtrl.getRuntimeRegistryInfo);
+router.get('/hyperledger/registries/:registryId', registryHyperledgerCtrl.getRuntimeRegistryInfo);
 
 module.exports = router;
